6bcd2d52fde7d9ee50b7d0f813e77b2bc8867748
rkavlev <r.kavlev@ritm.media>
2023-01-03 01:12:51 +0300

    modified configs.py added default conf_name

--- configs.py
+++ configs.py
@@ -5,6 +5,8 @@ import sys
 
 def load_conf():
     conf_name = os.environ.get("BOT_ENV")
+    if conf_name is None:
+        conf_name = "production"å
     try:
         return importlib.import_module(f"configs.{conf_name}")
     except (TypeError, ValueError, ImportError):
